793923243938f264b242ad03e270a1507841963e
ashikalmc <ashika7963@gmail.com>
2026-09-13 06:55:38 +0545

    add variable page

---
+++ python-basics/Variables.py
@@ -0,0 +1,13 @@
+#Variable
+fullName = "Ashika Lamichhane"
+age = 20
+rollno = 101
+study = "BCA"
+print("Full name is:",fullName)
+print("Full name is:",age)
+print("Full name is:",rollno)
+print("Full name is:",study)
+
+#Find out the type
+print(type(fullName))
+print(type(age))
