c02009ed44aeca5158c901a8e60c4c2fbb7e2f97
ashikalmc <ashika7963@gmail.com>
2026-09-13 07:41:08 +0545

    add page

---
+++ python-basics/datatype,operation.py
@@ -0,0 +1,81 @@
+#DataType
+book = "Atomic Habit"
+page = 209
+price = 99.99
+friction = True
+print(book)
+print(friction)
+print(type(friction))
+
+#Arthimatic Operation
+a = 25
+b = 55
+sum = a+b
+print("The Sum of two number:",sum)
+print("The Sum of two number:",a - b)
+print("The Sum of two number:",a * b)
+print("The Sum of two number:",a / b)
+print("The Sum of two number:",a % b)
+
+#Relational Operation
+firstnum = input("enter you firstnum:")
+secondnum = input("enter you secondnum:")
+num = firstnum == secondnum
+print(num)
+
+#Assignment Operator
+a = 5
+b >= a
+print(b)
+
+
+#Logical Operation
+a = 4
+b = 7
+sum = a + b 
+print (sum and b)
+
+#TypeCasting
+
+a = 5
+b = 5.9
+sum = a + b
+mul = a * b
+print(sum)
+
+# x = "a"
+# y = 2
+# sum = x + y
+# print(sum)
+
+x = float("7")
+y = 2.5
+sum = x + y
+print(sum)
+print(type(sum))
+
+
+# firstNum = int(input("Enter First Number:"))
+# SecondNum = int(input("Enter Second Number:"))
+# div = firstNum / SecondNum
+# print(div)
+
+
+name = input("Enter your name:")
+print(name)
+
+#Q1: WAP to enter two int num and print their sum
+num1 = int(input("enter first number:"))
+num2 = int(input("enter second number:"))
+sum = num1 + num2
+print("The sum of two int num is", sum)
+
+#Q2: WAP to input side of the square & print its area
+side = float(input("Enter side of the square:"))
+area = side * side
+print(area)
+
+ #Q3: WAP to input 2 int number, print true is a is greater or equal to b or print false
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+print(a >= b)
