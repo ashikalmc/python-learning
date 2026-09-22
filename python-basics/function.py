c7ece6b2bace9179841346c22035b6fa8cc0797d
ashikalmc <ashika7963@gmail.com>
2026-09-22 21:20:34 +0545

    Add the content of this page

--- python-basics/function.py
+++ python-basics/function.py
@@ -10,3 +10,64 @@ def get_greeting():
 message = get_greeting()
 print(message)
 
+# function defination
+def cal_sum(a,b): #parameters
+  sum = a+b
+  print(sum)
+  return sum
+#function call
+cal_sum(12,3) #arguments
+cal_sum(12,13)
+cal_sum(22,33)
+
+def cal_avg(a,b,c):
+  return a+b+c/3
+avg = cal_avg(22,33,45)
+print(avg)
+
+def greeding():
+   print("hello Ashika")
+
+greeding()
+
+def greeding():
+   print("hello Ashika")
+name = greeding()
+print(name) #none
+
+print("Hello Ashika", end="")
+print("lamichhane")
+
+#user-define function
+def cal_sum(a=1,b=2):
+  sum = a+b
+  print(sum)
+  return sum
+
+# cal_sum()
+# def cal_sum(b,a=2): #default value
+#   sum = a+b
+#   print(sum)
+#   return sum
+
+# cal_sum()
+
+#Q1 WAP to print the length of the list (list is the parameter)
+name = ["Ashika","Riju","Sushma","Amit"]
+def name_list(list):
+  print(len(list))
+
+print(len(name))
+
+#Q2 WAP to print the element of the list in single line (list is the parameter)
+favFlower = ["Rose","Sunflower","Lilly","Tulip","Lotus"]
+def flower(list):
+  print(list)
+
+print(favFlower)
+
+#Q3 WAP to find factorial of n (n is the parameter)
+def fact(n):
+  factorial = n*fact
+  print(factorial)
+fact()
